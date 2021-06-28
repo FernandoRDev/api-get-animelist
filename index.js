ec391e755fa2a0b8a8513b2683e1bdb6847c258a
fernandordev <fernandorodriguesdev@outlook.com>
2021-06-28 16:28:21 -0300

    small fix

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express()
 const PORT = process.env.PORT || 8080
 const router = require('./api/routes')
 
+app.get('/', (req, res) => {
+    res.send("How to use: https://github.com/FernandoRDev/api-get-animelist")
+})
+
 app.use('/api', router)
 
 app.listen(PORT, function () {
